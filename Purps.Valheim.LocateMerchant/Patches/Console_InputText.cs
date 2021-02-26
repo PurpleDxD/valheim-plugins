9e10caf2962020ee1a4fe9e83ddf0ec5ffe996a4
Justyn <purpledxd@gmail.com>
2021-02-26 00:38:29 -0500

    Initial Commit.

---
+++ Purps.Valheim.LocateMerchant/Patches/Console_InputText.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Purps.Valheim.LocateMerchant.Patches {
+    
+    [HarmonyPatch(typeof(Console), "InputText")]
+    public class Console_InputText {
+        [HarmonyPostfix]
+        public static void Postfix(Console __instance) {
+            if (Player.m_localPlayer == null) return;
+            if (__instance.m_input.text.ToUpper() != "LOCATEMERCHANT") return;
+            ZoneSystem.instance.FindClosestLocation("Vendor_BlackForest", Player.m_localPlayer.transform.position, out var locationInstance);
+            Minimap.instance.DiscoverLocation(locationInstance.m_position, Minimap.PinType.Icon3, "Merchant");
+            Console.instance.Print($"Found BlackForest Merchant! Location: {locationInstance.m_position}");
+        }
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+LocateMerchant - Valheim
+AUTHOR: Purps (https://steamcommunity.com/id/PurpsQQ/)
+
+:: REQUIREMENTS ::
+BepInEx for Valheim
+
+:: FEATURES ::
+• Pins the Blackforest Merchant on the Map!
+
+:: HOW TO USE ::
+• Open the Console (F5) and enter the command "LocateMerchant" once loaded in your World.
+
+:: CHANGELOG ::
+• Version 1.0.0
+    [NEW] Initial Commit
