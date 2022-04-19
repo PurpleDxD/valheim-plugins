c74086f4b33c74ebac3c1c8949d74d7894fbff26
Purps <purpledxd@gmail.com>
2022-04-18 23:23:59 -0400

    Add LocateMerchant

---
+++ LocateMerchant/LocateMerchantPlugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace Purps.Valheim.LocateMerchant {
+    [BepInPlugin("purps.valheim.locatemerchant", "Locate Merchant", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class Plugin : BaseUnityPlugin {
+        private const string pluginGuid = "purps.valheim.locatemerchant";
+        private const string pluginName = "Locate Merchant";
+        private const string pluginVersion = "1.0.0";
+
+        private void Awake() => new Harmony("purps.valheim.locatemerchant").PatchAll();
+    }
+}
---
+++ LocateMerchant/Patches/TerminalPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Purps.Valheim.LocateMerchant.Patches {
+    [HarmonyPatch(typeof(Terminal), "InputText")]
+    public class TerminalPatch {
+        private const Minimap.PinType pinType = Minimap.PinType.None;
+
+        [HarmonyPostfix]
+        public static void Postfix(Console __instance) {
+            if ((UnityEngine.Object)Player.m_localPlayer == (UnityEngine.Object)null || __instance.m_input.text.ToUpper() != "LOCATEMERCHANT")
+                return;
+            Game.instance.DiscoverClosestLocation("Vendor_BlackForest", Player.m_localPlayer.transform.position, "Merchant", 8);
+            Console.instance.Print(string.Format("Found BlackForest Merchant! Location: {0}", (object)((IEnumerable<Minimap.PinData>)Traverse.Create((object)Minimap.instance).Field("m_pins").GetValue()).First<Minimap.PinData>((Func<Minimap.PinData, bool>)(p => p.m_type == Minimap.PinType.None && p.m_name == "")).m_pos));
+        }
+    }
+}
